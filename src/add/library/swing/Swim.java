377f39535780122de1395c0731c5f5890105e3e4
DAM2T1 <polgalbarroaloy@gmail.com>
2018-03-08 19:51:41 +0100

    swing files added

---
+++ src/add/library/swing/Swim.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package add.library.swing;
+
+public class Swim {
+
+
+
+    public static void main(String[] args) {
+
+        Interfaz principal = new Interfaz();
+
+        // para centrarlo
+        principal.setLocationRelativeTo(null);
+
+        principal.setVisible(true);
+
+    }
+}
