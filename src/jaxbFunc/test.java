a7d6e2ce7d2d1ae2cba8b1be83b0627e7e854635
Unknown <edustrife@gmail.com>
2018-04-05 18:49:21 +0200

    Genero as ArrayList (singleton wraped)

--- src/jaxbFunc/test.java
+++ src/jaxbFunc/test.java
@@ -43,7 +43,7 @@ public class test {
                 linea = in.readLine();
                 libroActual.setAutor(linea);
                 linea = in.readLine();
-                libroActual.setGenero(linea);
+                libroActual.setGenero(Collections.singletonList(linea));
                 linea = in.readLine();
                 libroActual.setIdioma(linea);
                 linea = in.readLine();
